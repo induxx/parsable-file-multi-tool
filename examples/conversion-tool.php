c90d69b94532ce6763c35790a05e99d25a7054ce
Thijs De Paepe <thijs.dp@gmail.com>
2020-07-14 09:01:58 +0200

    added comments

--- examples/conversion-tool.php
+++ examples/conversion-tool.php
@@ -31,6 +31,7 @@ $references = [
 // encoders should receive the blueprints
 
 // source collection should receive the encoder
+// source collection should not know of the params of encoding or decoding.
 
 // collect all sources
 $sources = new \Misery\Component\Source\SourceCollection('akeneo/csv', $encoder, $akeneoBluePrintDir);
