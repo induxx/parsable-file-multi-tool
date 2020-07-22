a4a0bbe5940d2c50bb3dc3acbc78e0bc7f4fa36c
Thijs De Paepe <thijs.dp@gmail.com>
2020-07-22 16:59:05 +0200

    removed or moved all examples to the docs

--- examples/conversion-tool.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__.'/bootstrap.php';
-
-$converter = new Misery\Component\Converter\ItemConverter($encoder, $decoder, $actions);
-
-$converter->convertFromConfigurationFile(__DIR__.'/conversion.yaml');
