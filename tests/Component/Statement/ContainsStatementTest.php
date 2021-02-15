a38845d1acf6daed37e5f9252b4011ca3a6537e4
Thijs De Paepe <thijs.dp@gmail.com>
2021-02-15 13:43:45 +0100

    added another contains statement test

--- tests/Component/Statement/ContainsStatementTest.php
+++ tests/Component/Statement/ContainsStatementTest.php
@@ -28,6 +28,22 @@ class ContainsStatementTest extends TestCase
             'description' => 'LV',
             'sku' => '1234678',
         ], $statement->apply($item));
+
+        $item = [
+            'brand' => 'louis',
+            'description' => 'LV',
+            'sku' => 'LV-1234-678',
+        ];
+
+        $statement
+            ->when('sku', 'LV')
+            ->then('brand', 'Louis Vuitton');
+
+        $this->assertEquals([
+            'brand' => 'Louis Vuitton',
+            'description' => 'LV',
+            'sku' => 'LV-1234-678',
+        ], $statement->apply($item));
     }
 
     public function test_it_should_not_set_a_value_with_a_contains_statement(): void
