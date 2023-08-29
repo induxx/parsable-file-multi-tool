3f3701e748a11be5ec2d4f8bf227faa9b3265d75
Thijs De Paepe <thijs.dp@gmail.com>
2023-08-29 14:20:35 +0200

    fix: added contextFormatter

--- src/Component/Common/Pipeline/LoggingPipe.php
+++ src/Component/Common/Pipeline/LoggingPipe.php
@@ -2,14 +2,12 @@
 
 namespace Misery\Component\Common\Pipeline;
 
-use Misery\Component\Item\TypeGuesser;
-
 class LoggingPipe implements PipeInterface
 {
 
     public function pipe(array $item): array
     {
-        dump('Result', TypeGuesser::guess($item));
+        dump('Result', $item);
         return $item;
     }
 }
--- src/Component/Debugger/ItemDebugger.php
+++ src/Component/Debugger/ItemDebugger.php
@@ -2,12 +2,10 @@
 
 namespace Misery\Component\Debugger;
 
-use Misery\Component\Item\TypeGuesser;
-
 class ItemDebugger
 {
     public function log($item, $message)
     {
-        dump($message, TypeGuesser::guess($item));
+        dump($message, $item);
     }
 }
