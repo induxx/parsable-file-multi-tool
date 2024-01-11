8dfb1ba8d8782f43938006c866a3fb7e816a454f
Thijs De Paepe <thijs.dp@gmail.com>
2024-01-11 14:24:03 +0100

    added: frame to bootstrap

--- tests/Component/Action/FrameActionTest.php
+++ tests/Component/Action/FrameActionTest.php
@@ -55,7 +55,7 @@ class FrameActionTest extends TestCase
         ];
 
         $format->setOptions([
-            'fields' => ['brand', 'description',],
+            'fields' => ['brand', 'description'],
         ]);
 
         $this->assertEquals([
