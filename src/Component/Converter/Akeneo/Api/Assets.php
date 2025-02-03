71fbdd016e339bba8e34675f6f1f2c06be1acb77
Jonas De Gauquier <jonasdg@hotmail.be>
2025-02-03 13:39:25 +0100

    ✨ feat(VDS): add new converters for Akeneo Assets and Reference Entities
    Added `Assets` and `ReferenceEntities` classes to the Akeneo API
    component. These converters enhance data processing by allowing
    the conversion of assets and reference entities from Akeneo CSV
    format. This improves the overall flexibility and usability of
    the data handling process.

---
+++ src/Component/Converter/Akeneo/Api/Assets.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Misery\Component\Converter\Akeneo\Api;
+
+use Misery\Component\Common\Options\OptionsInterface;
+use Misery\Component\Common\Options\OptionsTrait;
+use Misery\Component\Common\Registry\RegisteredByNameInterface;
+use Misery\Component\Converter\AkeneoCsvHeaderContext;
+use Misery\Component\Converter\ConverterInterface;
+
+class Assets implements ConverterInterface, RegisteredByNameInterface, OptionsInterface
+{
+    use OptionsTrait;
+
+    private $csvHeaderContext;
+    private $options = [
+        'list' => null,
+    ];
+
+    public function __construct(AkeneoCsvHeaderContext $csvHeaderContext)
+    {
+        $this->csvHeaderContext = $csvHeaderContext;
+    }
+
+    public function convert(array $item): array
+    {
+        $this->csvHeaderContext->unsetHeader();
+        $separator = '-';
+        $output = [];
+
+        foreach ($item as $key => $value) {
+            if (in_array($key, ['code', 'labels', '%asset_family_code%'])) {
+                continue;
+            }
+
+            $keys = explode($separator, $key);
+            # values
+            $prep = $this->csvHeaderContext->create($item)[$key];
+
+            $prep['channel'] = $prep['scope'];
+            unset($prep['scope']);
+
+            $prep['data'] = $value;
+            unset($prep['key']);
+
+            $output['values'][$keys[0]][] = $prep;
+            unset($item[$key]);
+        }
+
+        return $item+$output;
+    }
+
+    public function revert(array $item): array
+    {
+       return $item;
+    }
+
+    public function getName(): string
+    {
+        return 'akeneo/assets/api';
+    }
+}
