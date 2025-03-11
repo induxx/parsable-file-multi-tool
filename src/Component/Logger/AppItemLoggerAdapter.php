bb9651c8f17d98de9b4fa86e832cb0f512469c86
Thijs De Paepe <thijs.dp@gmail.com>
2025-03-11 19:07:56 +0100

    rename: log to tracker

--- src/Component/Logger/AppItemLoggerAdapter.php
+++ src/Component/Logger/AppItemLoggerAdapter.php
@@ -2,34 +2,40 @@
 
 namespace Misery\Component\Logger;
 
+use App\Component\ItemTracker\ItemTrackerInterface;
 use App\Component\Logger\ItemLogger;
 
 class AppItemLoggerAdapter implements ItemLoggerInterface
 {
-    public function __construct(public readonly \App\Component\Logger\ItemLoggerInterface $logger) {}
+    public function __construct(private readonly ItemTrackerInterface $tracker) {}
 
     public function logCreate(string $entityClass, string $identifier): void
     {
-        $this->logger->logCreate($entityClass, $identifier);
+        $this->tracker->trackCreate($entityClass, $identifier);
     }
 
     public function logUpdate(string $entityClass, string $identifier, string $updateMessage = ''): void
     {
-        $this->logger->logUpdate($entityClass, $identifier, $updateMessage);
+        $this->tracker->trackUpdate($entityClass, $identifier, $updateMessage);
     }
 
     public function logRemove(string $entityClass, string $identifier): void
     {
-        $this->logger->logRemove($entityClass, $identifier);
+        $this->tracker->trackRemove($entityClass, $identifier);
     }
 
     public function logFailed(string $entityClass, string $identifier, string $failMessage): void
     {
-        $this->logger->logFailed($entityClass, $identifier, $failMessage);
+        $this->tracker->trackFailed($entityClass, $identifier, $failMessage);
+    }
+
+    public function logSkipped(string $entityClass, string $identifier, string $failMessage): void
+    {
+        $this->tracker->trackSkipped($entityClass, $identifier, $failMessage);
     }
 
     public function log(string $entityClass, string $identifier): void
     {
-        $this->logger->log($entityClass, $identifier);
+        $this->tracker->track($entityClass, $identifier);
     }
 }
