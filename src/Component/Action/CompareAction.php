a1d71af739792ea06182a2d8ff9c2d80e9f1a446
Jonas De Gauquier <jonasdg@hotmail.be>
2025-05-19 11:06:56 +0200

    ✨ feat(compare_action): add CompareAction for field comparison logic
    This new action allows for comparing fields in items based on
    specified criteria. It supports both equality and inequality checks,
    enabling dynamic responses based on comparison results. This enhances
    the flexibility of the action handling within the application.

---
+++ src/Component/Action/CompareAction.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Misery\Component\Action;
+
+use Misery\Component\Common\Options\OptionsInterface;
+use Misery\Component\Common\Options\OptionsTrait;
+use Misery\Component\Common\Utils\ValueFormatter;
+
+class CompareAction implements ActionInterface, OptionsInterface
+{
+    use OptionsTrait;
+
+    public const NAME = 'compare';
+
+    private $options = [
+        'field'         => null,
+        'compare_field' => null,
+        'state'         => '',
+        'skip_message'  => '',
+        'then'          => [],
+    ];
+
+    private array $values = [];
+
+    public function apply(array $item): array
+    {
+        $field = $this->getOption('field');
+        $compareField = $this->getOption('compare_field');
+        $state = $this->getOption('state');
+        $then = $this->getOption('then');
+        $thenAction = $this->getThenAction($then, $item);
+
+        if ($thenAction === null) {
+            return $item;
+        }
+
+        $value = $item[$field] ?? null;
+        $compareValue = $item[$compareField] ?? null;
+
+        if (gettype($value) !== gettype($compareValue)) {
+            return $item;
+        }
+
+        if ($state === 'EQUALS') {
+            if (is_array($value) && is_array($compareValue)) {
+                if (count($value) !== count($compareValue)) {
+                    return $item;
+                }
+
+                sort($value);
+                sort($compareValue);
+
+                if ($value === $compareValue) {
+                    return $thenAction->apply($item);
+                }
+
+                return $item;
+            }
+
+            if ($value === $compareValue) {
+                $thenAction->apply($item);
+            }
+        }
+
+        if ($state === 'NOT_EQUALS') {
+            if (is_array($value) && is_array($compareValue)) {
+                if (count($value) !== count($compareValue)) {
+                    return $thenAction->apply($item);
+                }
+
+                sort($value);
+                sort($compareValue);
+
+                if ($value !== $compareValue) {
+                    return $thenAction->apply($item);
+                }
+
+                return $item;
+            }
+
+            if ($value !== $compareValue) {
+                return $thenAction->apply($item);
+            }
+        }
+
+        return $item;
+    }
+
+    public function __destruct()
+    {
+        $this->values = [];
+    }
+
+    private function getThenAction(
+        array $then,
+              $item
+    ): CombineAction|SkipAction|CopyAction|DebugAction|RemoveAction|null {
+        $action = null;
+        if (isset($then['action'])) {
+            if ($then['action'] === 'skip') {
+                $action = new SkipAction();
+                $message = $then['skip_message'] ?? '';
+                if (!empty($message)) {
+                    $message = ValueFormatter::format($message, $item);
+                }
+                $action->setOptions(['skip_message' => $message, 'force_skip' => true]);
+            }
+            if ($then['action'] === 'copy') {
+                $action = new CopyAction();
+                $action->setOptions($then);
+            }
+            if ($then['action'] === 'debug') {
+                $action = new DebugAction();
+            }
+
+            if ($then['action'] === 'remove') {
+                $action = new RemoveAction();
+                $action->setOptions($then);
+            }
+
+            if ($then['action'] === 'combine') {
+                $action = new CombineAction();
+                $action->setOptions($then);
+            }
+        }
+
+        return $action;
+    }
+}
